3f58c46650411defe768d8a6b6e2da2d0118ced3
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-11-22 16:49:29 +0100

    Testing first feature

---
+++ cmd/test/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"time"
+
+	goquickdata "github.com/CaptainFallaway/GoQuickData"
+)
+
+func main() {
+	wholeTestStart := time.Now()
+	file, err := goquickdata.OpenFileMmap("./testdata/yellow_tripdata_2015-03.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fileOpenTime := fmt.Sprintf("Took %v To Open File", time.Since(wholeTestStart))
+
+	actual := time.Now()
+
+	scanner := file.GetScanner()
+
+	scanner.Split(bufio.ScanLines)
+	scanner.Text()
+
+	i := 0
+
+	for scanner.Scan() {
+		scanner.Text()
+		i++
+	}
+
+	fmt.Printf("%v Lines\n%v\n", i, fileOpenTime)
+	fmt.Printf("Reading Lines Took %v\n", time.Since(actual))
+	fmt.Printf("Whole Test Took %v", time.Since(wholeTestStart))
+
+	for {
+	}
+}
